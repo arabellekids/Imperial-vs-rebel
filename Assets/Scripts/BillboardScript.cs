786a625980be5f5183ccf273bf051a50a96fca42
Aaron Smith <arabellekids@gmail.com>
2020-01-23 18:27:16 -0500

    Added targeting and start scene

---
+++ Assets/Scripts/BillboardScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardScript : MonoBehaviour
+{
+    public Transform targetCam;
+    public int enemyPlayerNum;
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetCam = GameObject.Find("Player "+enemyPlayerNum+" cam").transform;
+        if (targetCam != null)
+        {
+            transform.LookAt(targetCam.position);
+        }
+    }
+}
---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string SceneName;
+    public void LoadGameScene()
+    {
+        SceneManager.LoadSceneAsync(SceneName);
+    }
+}
